02802d5c787eb068772710bcf3fe69188af6ea68
karavaevatasha <karavaeva502@gmail.com>
2023-11-20 08:26:11 -0600

    Initial Commit

---
+++ AsyncPhotoLoader/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AsyncPhotoLoader
+//
+//  Created by Natalie on 2023-11-20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
